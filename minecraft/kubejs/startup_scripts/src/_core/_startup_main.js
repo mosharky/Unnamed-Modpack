15488a29b44924db22ddb90129b2942b0b977e4c
mo_shark <medoelzarkalt@gmail.com>
2025-06-04 07:13:40 -0400

    Fixed Ingredient only holding vanilla items

--- minecraft/kubejs/startup_scripts/src/_core/_startup_main.js
+++ minecraft/kubejs/startup_scripts/src/_core/_startup_main.js
@@ -1,17 +1,18 @@
 // priority: -1
 
-// Because not all KubeJS methods support regex
-global.REMOVALS.all.forEach(entry => {
-    if (entry instanceof RegExp) {
-        Ingredient.of(entry).itemIds.forEach(itemId => global.REMOVALS.add(itemId))
-        global.REMOVALS.all.delete(entry)
-    }
+// postInit because Ingredient needs to wait for mods to load in order to hold anything other than vanilla items
+StartupEvents.postInit(e => {
+    // Because not all KubeJS methods support regex
+    global.REMOVALS.all.forEach(entry => {
+        if (entry instanceof RegExp) {
+            Ingredient.of(entry).itemIds.forEach(itemId => global.REMOVALS.add(itemId))
+            global.REMOVALS.all.delete(entry)
+        }
+    })
+
+    // Changing blockswap config with KubeJS!
+    processBlockswapConfig()
+
+    console.log('REMOVALS:')
+    console.log(global.REMOVALS.getAsArray())
 })
-
-// Changing blockswap config with KubeJS!
-processBlockswapConfig()
-
-// console.log(Ingredient.of('*').itemIds)
-
-console.log('REMOVALS:')
-console.log(global.REMOVALS.getAsArray())
