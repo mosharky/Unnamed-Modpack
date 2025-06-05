41dc155296f2f921ce4f1ecfcf0c50d57e603cd5
mo_shark <medoelzarkalt@gmail.com>
2025-06-04 22:22:52 -0400

    Fixed Quark removals

--- minecraft/kubejs/startup_scripts/src/quark/removals.js
+++ minecraft/kubejs/startup_scripts/src/quark/removals.js
@@ -1,7 +1,14 @@
-global.REMOVALS.add([
-    /quark:.*chest.*/,
-    /quark:.*blossom.*/,
-    /quark:.*ancient(?!_tome).*/,
-    /quark:.*azalea(?!.*hedge).*/,
-    /quark:.*permafrost.*/,
-])
+function quarkRemovals() {
+    global.REMOVALS.add([
+        /quark:(.*chest.*|.*ladder.*|.*bookshelf.*|.*leaf_carpet.*)/,
+        /quark:.*thatch.*/,
+        // TODO: Remove woods from compat stuff (Supplementaries, everycomp, etc)
+        /quark:.*blossom.*/,
+        /quark:.*ancient(?!_tome).*/,
+        /quark:.*azalea(?!_hedge).*/,
+        /quark:.*permafrost.*/,
+        'quark:gold_bars',
+        'quark:crate',
+        'quark:chute'
+    ])
+}
