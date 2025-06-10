fd2bafa38896768066c7c3453ddfc25be70e08db
mo_shark <medoelzarkalt@gmail.com>
2025-06-10 00:57:51 -0400

    Removed Environemtnal Wisteria

--- minecraft/kubejs/server_scripts/src/environmental/worldgen.js
+++ minecraft/kubejs/server_scripts/src/environmental/worldgen.js
@@ -5,9 +5,12 @@ function environmentalWorldgen(e) {
 
     // Disable biome modifiers
     removeBiomeModifier(e, 'environmental:add_feature/cattails')
-    removeBiomeModifier(e, 'environmental:add_feature/swamp_vegetation')
     removeBiomeModifier(e, 'environmental:remove_feature/swamp_oak')  // not sure if this is even needed
 
+    removeBiomeModifier(e, 'environmental:add_feature/flower_forest_vegetation') 
+    addFeatures(e, 'environmental:patch_delphiniums', 'minecraft:flower_forest', 'vegetal_decoration')
+
+    removeBiomeModifier(e, 'environmental:add_feature/swamp_vegetation')
     addFeatures(e, 'environmental:patch_duckweed_swamp', [
         'minecraft:swamp',
         'natures_spirit:marsh'
