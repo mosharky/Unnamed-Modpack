fee55c1e502d5bb71da88934f0ed536dedc26af1
mo_shark <medoelzarkalt@gmail.com>
2025-11-29 20:19:57 -0500

    More Atmospheric integration

--- minecraft/kubejs/server_scripts/src/atmospheric/tags.js
+++ minecraft/kubejs/server_scripts/src/atmospheric/tags.js
@@ -1,3 +1,44 @@
 /** @param {$TagEventJS_} e */
 function biomeTags_Atmospheric(e) {
+    e.add('kubejs:has_feature/atmospheric_aspen_parkland_foliage', [
+        'natures_spirit:aspen_forest'
+    ])
+
+    e.add('kubejs:has_feature/atmospheric_patch_arid_sprouts', [
+        'natures_spirit:lively_dunes',
+        'natures_spirit:blooming_dunes',
+        'natures_spirit:stratified_desert',
+        'natures_spirit:scorched_dunes',
+        'natures_spirit:drylands',
+        'natures_spirit:arid_highlands',
+        'natures_spirit:arid_savanna',
+    ])
+
+    e.add('kubejs:has_feature/atmospheric_patch_yucca_flower', [
+        'natures_spirit:lively_dunes',
+        'natures_spirit:blooming_dunes',
+        'natures_spirit:drylands',
+        'natures_spirit:wooded_drylands',
+        'natures_spirit:arid_highlands',
+        'natures_spirit:arid_savanna',
+    ])
+
+    e.add('kubejs:has_feature/atmospheric_patch_aloe_vera', [
+        'natures_spirit:lively_dunes',
+        'natures_spirit:blooming_dunes',
+        'natures_spirit:stratified_desert',
+        'natures_spirit:scorched_dunes',
+    ])
+
+    e.add('kubejs:has_feature/atmospheric_patch_barrel_cactus_dunes', [
+        'natures_spirit:lively_dunes',
+        'natures_spirit:blooming_dunes',
+        'natures_spirit:stratified_desert',
+        'natures_spirit:scorched_dunes',
+    ])
+
+    e.add('kubejs:has_feature/atmospheric_patch_agave_large', [
+        'natures_spirit:lively_dunes',
+        'natures_spirit:blooming_dunes',
+    ])
 }
--- minecraft/kubejs/server_scripts/src/atmospheric/worldgen.js
+++ minecraft/kubejs/server_scripts/src/atmospheric/worldgen.js
@@ -9,7 +9,43 @@ STRUCTURE_BLOCK_SWAPPER.set('atmospheric:kousa_sanctum', new Map([
 
 /** @param {$DataPackEventJS_} e  */
 function worldgen_Atmospheric(e) {
-    
+
     // Disable Aspen Parkland biome
     e.addJson('atmospheric:blueprint/modded_biome_slices/aspen', disableBiomeSliceJson)
+
+    addFeatures(e,
+        [
+            copyPasteFeature(e, 'atmospheric', PLACED, 'atmospheric:crustose'),
+            copyPasteFeature(e, 'atmospheric', PLACED, 'atmospheric:fallen_crustose_log'),
+            copyPasteFeature(e, 'atmospheric', PLACED, 'atmospheric:single_agave'),
+            copyPasteFeature(e, 'atmospheric', PLACED, 'atmospheric:patch_agave_large'),
+            copyPasteFeature(e, 'atmospheric', PLACED, 'atmospheric:patch_golden_growths'),
+        ], '#kubejs:has_feature/atmospheric_aspen_parkland_foliage',
+        'vegetal_decoration'
+    )
+    addFeatures(e,
+        copyPasteFeature(e, 'atmospheric', PLACED, 'atmospheric:patch_arid_sprouts'),
+        '#kubejs:has_feature/atmospheric_patch_arid_sprouts',
+        'vegetal_decoration'
+    )
+    addFeatures(e,
+        copyPasteFeature(e, 'atmospheric', PLACED, 'atmospheric:patch_yucca_flower'),
+        '#kubejs:has_feature/atmospheric_patch_yucca_flower',
+        'vegetal_decoration'
+    )
+    addFeatures(e,
+        copyPasteFeature(e, 'atmospheric', PLACED, 'atmospheric:patch_aloe_vera'),
+        '#kubejs:has_feature/atmospheric_patch_aloe_vera',
+        'vegetal_decoration'
+    )
+    addFeatures(e,
+        copyPasteFeature(e, 'atmospheric', PLACED, 'atmospheric:patch_barrel_cactus_dunes'),
+        '#kubejs:has_feature/atmospheric_patch_barrel_cactus_dunes',
+        'vegetal_decoration'
+    )
+    addFeatures(e,
+        copyPasteFeature(e, 'atmospheric', PLACED, 'atmospheric:patch_agave_large'),
+        '#kubejs:has_feature/atmospheric_patch_agave_large',
+        'vegetal_decoration'
+    )
 }
